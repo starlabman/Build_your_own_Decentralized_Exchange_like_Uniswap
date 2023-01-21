f4f538c56790c9500ea72eaef354bad1cd7efdad
labore <akodjolabore@gmail.com>
2023-01-21 13:18:58 +0000

    add

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xFD657d520b64F69f4416bAcE7a83c508Bb015430";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
